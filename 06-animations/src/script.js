cf2808e487877f96c209d5908b9838efd1270e7e
simonhuwiler <webmaster@simonhuwiler.ch>
2021-04-12 16:04:22 +0200

    wip

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -7,42 +7,36 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Objects
- */
+// Object
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-mesh.position.x = 0.5;
-mesh.position.y = -0.5;
-
-const axesHelper = new THREE.AxesHelper(3)
-scene.add(axesHelper)
-
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
     width: 800,
     height: 600
 }
 
-/**
- * Camera
- */
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-// camera.position.y = 1;
-// camera.position.x = 1;
 scene.add(camera)
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+const tick = () => {
+
+    mesh.position.x += 0.01;
+
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
